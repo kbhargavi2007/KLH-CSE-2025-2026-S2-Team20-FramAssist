4f3bdb3118b323627aa75adae0ec301b88e02963
K Bhargavi <kvbhargavi2007@gmail.com>
2026-08-27 11:25:29 +0530

    Add files via upload

---
+++ Farm_Assist/CorpusReader.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class CorpusReader {
+
+    /*
+     * Reads one corpus file and returns its complete text.
+     */
+    public static String readFile(File file) {
+
+        StringBuilder content = new StringBuilder();
+
+        try {
+
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+
+                content.append(scanner.nextLine());
+                content.append("\n");
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+
+            System.out.println(
+                    "Unable to read file: " + file.getName());
+
+            return "";
+        }
+
+        return content.toString();
+    }
+}
